4e8e6a2aee5d80e55263decff5c81bfde8d160f6
steden <1470804@qq.com>
2023-03-06 15:17:02 +0800

    修改注释

--- setProfiles.go
+++ setProfiles.go
@@ -8,6 +8,9 @@ import (
 )
 
 // SetProfiles 设置内存缓存（集合）
+// key：这批数据的key，用于区分不同数据集合。
+// uniqueField：数据集合中，用于区分Item的唯一值的字段名称（主键）
+// expiry：缓存失效时间
 func SetProfiles[TEntity any](key string, uniqueField string, expiry time.Duration) cache.ICacheManage[TEntity] {
 	if uniqueField == "" {
 		exception.ThrowRefuseException("缓存集合数据时，需要设置UniqueField字段")
