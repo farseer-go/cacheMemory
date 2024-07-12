4fd2550a2b3f7b85153864022cd8bef09d0398b1
steden <1470804@qq.com>
2024-07-12 23:24:03 +0800

    新增得到所有缓存项

--- cacheList.go
+++ cacheList.go
@@ -1,5 +1,7 @@
 package cacheMemory
 
+import "github.com/farseer-go/collections"
+
 type CacheList[T any] struct {
 	items map[string]*T
 }
@@ -23,4 +25,13 @@ func (receiver *CacheList[T]) SetIfNotExists(key string, getItem func() *T) {
 // Get 获取数据
 func (receiver *CacheList[T]) Get(key string) *T {
 	return receiver.items[key]
+}
+
+// ToList 得到所有缓存项
+func (receiver *CacheList[T]) ToList() collections.List[T] {
+	lst := collections.NewList[T]()
+	for _, t := range receiver.items {
+		lst.Add(*t)
+	}
+	return lst
 }
